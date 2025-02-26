5bc3cd51efe25b68feba07410c5d35883bde9ecd
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-26 22:32:19 +0000

    Exercicio 5 Solicitação e conversão

---
+++ Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Insere a altura do cilindro;");
+            string str1 = Console.ReadLine();
+            double a = double.Parse(str1);
+
+            Console.WriteLine("Insere o raio do cilindro;");
+            string str2 = Console.ReadLine();
+            double r = double.Parse(str2);
+            
+        }
+    }
+}
