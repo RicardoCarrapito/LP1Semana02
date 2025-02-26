8e7974f1137ff4bb8ae97580a4736b95a661e412
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-26 22:22:06 +0000

    Exercico 4

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número-não negativo:");
+            string str= Console.ReadLine();
+            sbyte n= sbyte.Parse(str);
+
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^6);
+            Console.WriteLine(n>10);
+            
+
+        }
+    }
+}
