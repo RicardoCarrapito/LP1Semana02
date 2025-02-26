a94325be5ba0bc16293a1944c39cede9485957b2
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-26 23:26:32 +0000

    Exercicio 6 primeira parte

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            byte b1 =byte.MaxValue;
+            byte b2=byte.MinValue;
+
+            sbyte sb1=sbyte.MaxValue;
+            sbyte sb2=sbyte.MinValue;
+
+            short sh1= short.MaxValue;
+            short sh2=short.MinValue;
+
+            ushort ush1= ushort.MaxValue;
+            ushort ush2= ushort.MinValue;
+
+            int i1= int.MaxValue;
+            int i2= int.MinValue;
+
+            uint ui1=uint.MaxValue;
+            uint ui2=uint.MinValue;
+
+            long l1=long.MaxValue;
+            long l2=long.MinValue;
+
+            ulong ul1=ulong.MaxValue;
+            ulong ul2=ulong.MinValue;
+
+            char ch1=char.MaxValue;
+            char ch2=char.MinValue;
+
+            float fl1=float.MaxValue;
+            float fl2=float.MinValue;
+
+            double db1=double.MaxValue;
+            double db2=double.MinValue;
+
+            decimal de1=decimal.MaxValue;
+            decimal de2=decimal.MinValue;
+
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+            Console.WriteLine(sb1);
+            Console.WriteLine(sb2);
+            Console.WriteLine(sh1);
+            Console.WriteLine(sh2);
+            Console.WriteLine(ush1);
+            Console.WriteLine(ush2);
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
+            Console.WriteLine(ui1);
+            Console.WriteLine(ui2);
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+            Console.WriteLine(ul1);
+            Console.WriteLine(ul2);
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.WriteLine(fl1);
+            Console.WriteLine(fl2);
+            Console.WriteLine(db1);
+            Console.WriteLine(db2);
+            Console.WriteLine(de1);
+            Console.WriteLine(de2);
+
+
+        }
+    }
+}
