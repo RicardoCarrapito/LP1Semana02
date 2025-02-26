f5d65601026ab030f8b2d0c91090783db20cbedf
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-26 22:08:17 +0000

    exercicio 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza um numero inteiro:");
+            string str=Console.ReadLine();
+            sbyte n= sbyte.Parse(str);
+            n--;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+        }
+    }
+}
