1208e92924b4abb27e8dbabd369ffe614d71f2f2
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-27 16:02:05 +0000

    exercicio 10 tive problemas tive que começar de novo

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string question;
+            do
+            {
+                Console.WriteLine("Insira uma questão:");
+                question = Console.ReadLine();
+
+
+                if(question=="EXIT")
+                {
+                    break;
+                }
+                
+                else if (question == "És mais inteligente que eu?")
+                {
+                    Console.WriteLine("Claro que sim");
+                }
+
+                else if (question == "Sabes falar quantas linguas?")
+                {
+                    Console.WriteLine("TODAS");
+                }
+
+                else if (question == "Quantos anos tens?")
+                {
+                    Console.WriteLine("Mais do que possas contar");
+                }
+
+                else if (question == "Tens algum género?")
+                {
+                    Console.WriteLine("Não, não tenho nenhum género definido");
+                }
+                else
+                {
+                    Console.WriteLine("Não tenho resposta");
+                }
+
+
+
+            } while (question != "EXIT");
+        }
+    }
+}
