dccd0bbabad94e97b894e00c8b0f808a22c9f71d
Ricardo Carrapito <ricardocarrapito.dev@gmail.com>
2025-02-28 14:51:09 +0000

    finalização de exercicio

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -6,40 +6,45 @@ namespace PerfectCountdown
     {
         private static void Main(string[] args)
         {
-            
+
             int start;
             int step;
+            int x;
             do
             {
-            Console.WriteLine("Start number:");
-            string str1=Console.ReadLine();
-             start=int.Parse(str1);
+                Console.WriteLine("Start number:");
+                string str1 = Console.ReadLine();
+                start = int.Parse(str1);
 
-            Console.WriteLine("Step number:");
-            string str2 = Console.ReadLine();
-             step=int.Parse(str2);
-
-
-            if(start<1)
-            {
-                Console.WriteLine("Out-of-range start or step. Try again.");
-            }
-            else if(step<1)
-            {
-                Console.WriteLine("Out-of-range start or step. Try again.");
-            }
-            else if(step>=start)
-            {
-                Console.WriteLine("Start must be higher than step. Try again.");
-            }
-            
-            else if(start)
-            {
-
-            }
+                Console.WriteLine("Step number:");
+                string str2 = Console.ReadLine();
+                step = int.Parse(str2);
 
 
-            } while (start!=0);
+                if (start < 1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                }
+                else if (step < 1)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                }
+                else if (step >= start)
+                {
+                    Console.WriteLine("Start must be higher than step. Try again.");
+                }
+
+                else if (start % step > 0)
+                {
+                    Console.WriteLine("Start not divisible by step. Try again.");
+                }
+                else
+                {
+                     x =start-step;
+                    Console.WriteLine(x);
+                }
+
+            } while (start >= 0);
 
         }
     }
